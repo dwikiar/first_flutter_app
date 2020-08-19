81c1363050d8c556314a4e6fc650f71f63ac0f90
dwikiarh <dwikiauliar@gmail.com>
2020-08-19 13:49:20 +0700

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class Home extends StatelessWidget {
       ),
       body: Center(
         child: Text(
-          "Hello World!",
+          "Hello World!!!",
           style: TextStyle(
             fontSize: 20.0,
             fontWeight: FontWeight.bold,
